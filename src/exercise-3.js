f83fc1a4b34af9dd0d620d7cf37bb7b8ecf762aa
Pablo Mendez <mendez.developer@gmail.com>
2018-11-05 10:26:36 -0300

    updated exercise 3

--- src/exercise-3.js
+++ src/exercise-3.js
@@ -7,7 +7,19 @@ function getLongestName(countries) {
   }, '');
 }
 
+//ES6
+const getLongestNameES6 = countries => {
+  return countries.reduce((prev, current) => {
+    if (current.length > prev.length) {
+      prev = current;
+    }
+    return prev;
+  });
+}
+
 var result = getLongestName(['Australia', 'Germany', 'United States of America']);
+var resultES6 = getLongestNameES6(['Australia', 'Germany', 'United States of America']);
 
 console.log('"' + result + '" is the longest name.')
+console.log('"' + resultES6 + '" is the longest name.')
 
